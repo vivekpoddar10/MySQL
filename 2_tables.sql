ca66f17621d4334b77f931b0d48f822a98d14b22
vivekpoddar10 <vivek.kumar.poddar.01@gmail.com>
2023-11-04 19:58:32 +0530

    Update 2_tables.sql

--- 2_tables.sql
+++ 2_tables.sql
@@ -34,4 +34,20 @@ INSERT INTO petshop (name, age, species)
 VALUES
 	('Oscar', 3, 'Dog'),
     ('Blue', 4, 'Cat');
-    
+
+-- not null and default values
+-- NOT NULL is used to state that the column can't be empty, it is must to have a value
+-- DEFAULT <value> is used to specify the <value> if user doesn't enter any data inside the column
+-- PRIMARY KEY is used to uniquly identify the rows inside the column
+-- AUTO_INCREMENT is used to automatically increase the values
+-- e.g.
+CREATE TABLE employee (
+	id int PRIMARY KEY AUTO_INCREMENT, -- there will be unique id for each row, ant it can't be null, and it will automatically increase
+    first_name VARCHAR(10) NOT NULL, -- these column will must have data, can't be left empty
+    middle_name VARCHAR(10),
+    last_name VARCHAR(10) NOT NULL,
+    age INT NOT NULL DEFAULT 22, -- if user doesn't enter any value, then the default value will be shown in the column
+    current_status VARCHAR(10) NOT NULL DEFAULT 'Employeed'
+);
+
+
