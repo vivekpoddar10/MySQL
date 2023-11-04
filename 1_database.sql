9224bc8be63e5f0bb08fc1e0633b4b96467df3f4
vivekpoddar10 <vivek.kumar.poddar.01@gmail.com>
2023-11-04 16:17:14 +0530

    Create 1_database.sql
    Learnt how to create, use and delete the database

---
+++ 1_database.sql
@@ -0,0 +1,29 @@
+/**
+Syntax for showing database
+	SHOW DATABASES
+*/
+SHOW DATABASES;
+
+/**
+Syntax for creating database
+	CREATE DATABASE <database_name>
+*/
+CREATE DATABASE MySQLCodes;
+
+/**
+Syntax for deleting database
+	DROP DATABASE <database_name>
+*/
+DROP DATABASE mydatabase;
+
+/**
+Syntax for using the database
+	USE <database_name>
+*/
+use mysqlcodes;
+
+/** 
+Syntax for displaying your current database
+	SELECT database()
+**/
+SELECT database();
